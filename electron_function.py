f86d79e13fd2763e4a1029ea642cb057dbe68709
Mehdi <mehdibekkali24@augustana.edu>
2025-06-29 21:04:59 -0500

    Update electron_function.py

--- electron_function.py
+++ electron_function.py
@@ -7,6 +7,6 @@ def calculate_age_from_year(year):
 def calculate_age():
     year = int(input("Enter the year you were Born: "))
     age = calculate_age_from_year(year)
-    print(f"Your age is {age} years")
+    print(f"Your age is {age} yearssssssss")
 
 calculate_age()
